9a0d7c0415b4e60c09fec8d1e46da8404f395128
Mohit Sharma <mohitsharma603@gmail.com>
2018-07-07 18:46:46 +0530

    Initial Commit

---
+++ src/main/java/com/kinley/SpringBootIntialApplication.java
@@ -0,0 +1,12 @@
+package com.kinley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntialApplication.class, args);
+	}
+}
